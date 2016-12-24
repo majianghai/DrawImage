1b8d33f635680651adf783bdcd1d01026f091628
majianghai <635289016@qq.com>
2016-12-24 17:15:55 +0800

    代码的第一次初始化

---
+++ DrawImage/DrawImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawImage
+//
+//  Created by majianghai on 2016/12/24.
+//  Copyright © 2016年 majianghai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
